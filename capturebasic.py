f560b09d5babe38324fd0fe6c6b1ce9e8efac42c
Kieleth <kieleth@gmail.com>
2014-03-26 00:13:34 +0000

    functionalize part II, bloody enforcer and its counters, find way to do this right

--- capturebasic.py
+++ capturebasic.py
@@ -33,58 +33,86 @@ flags = cv2.CASCADE_SCALE_IMAGE
 minSize = (200, 200)
 maxSize = None # (300, 300)
 
-while(True):
-    #Time tracking w opencv:
-    frame_tick_start = cv2.getTickCount()
-
-    #Capture frame-by-frame
-    ret, frame = capture.read()
-    if frame is None:
-        raise
-
-    #flips horizontally:
+def flip_frame(frame):
+    #1 flips horizontally:
     #frame = np.fliplr(frame) # check which is faster:
-    frame = cv2.flip(frame, 1)
+    return cv2.flip(frame, 1)
 
-    # Our operations on the frame come here
+def process_frame_for_capture(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.equalizeHist(gray)
+    return gray
 
-    #Recognition:
-    faces = face_cascade.detectMultiScale(gray, scale_factor, min_neigh,minSize=minSize, maxSize=maxSize, flags=flags)
+def detect_faces(frame, scale_factor, min_neigh, minSize, maxSize, flags):
+    faces_list = face_cascade.detectMultiScale(
+                                  frame,
+                                  scale_factor,
+                                  min_neigh,
+                                  minSize=minSize,
+                                  maxSize=maxSize,
+                                  flags=flags)
     #LOG>print faces
+    return faces_list
 
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
+def display_faces(cv_img, faces_list):
+    #faces_list has format [(x, y, w, h), ..]
+    for (x, y, w, h) in faces_list:
+        cv2.rectangle(cv_img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        #roi_gray = gray[y:y + h, x:x + w]
+        #roi_color = frame[y:y + h, x:x + w]
+        display_rectangle_coords(cv_img, x, y, w, h)
 
-        #displays coords on screen
-        cv2.putText(frame, "pos(x, y)=(%s,%s)" % (x, y), (x + w + 10, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
-        cv2.putText(frame, "size(w x h)=(%sx%s)" % (w, h), (x + w + 10, y + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+def display_rectangle_coords(cv_image, x, y, w, h):
+    #displays coords on screen
+    cv2.putText(cv_image, "pos(x, y)=(%s,%s)" % (x, y), (x + w + 10, y + 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
 
+    cv2.putText(cv_image, "size(w x h)=(%sx%s)" % (w, h), (x + w + 10, y + 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255))
+
+def behave_enforces(face):
+    enforce_face_within(face, x_limit_low, x_limit_high, y_limit_low, y_limit_high)
+ 
+def enforce_face_within(face, x_limit_low, x_limit_high, y_limit_low, y_limit_high):
     # FACE POSITION CONTROL:
-    #assuming one face:
-    if len(faces) == 1:
-        face_rect = faces[0]
+    x, y, w, h = face
 
-        x, y, w, h = face_rect
-        #>LOG>print 'face is positioned at x=%s y=%s' % (x, y)
-        if x < x_limit_low or x > x_limit_high or y > y_limit_low or y < y_limit_high:
-            counter += 1
-            print 'warning... :>'
-    
-            if counter == COUNTER:
-                print 'You are doing something wrong!!!'
-                say()
-                counter = 0
-        elif counter > 0:
-            counter_ok += 1
+    if x < x_limit_low or x > x_limit_high or y > y_limit_low or y < y_limit_high:
+        counter += 1
+        print 'warning... :>'
+
+        if counter == COUNTER:
+            print 'You are doing something wrong!!!'
+            say()
+            counter = 0
+
+    elif counter > 0:
+        counter_ok += 1
     
     if counter_ok == 10:
         print 'counter_ok is RESET'
         counter_ok = 0
         counter = 0
+
+while(True):
+    #Time tracking w opencv:
+    frame_tick_start = cv2.getTickCount()
+
+    #Capture frame-by-frame
+    _, the_frame = capture.read()
+    if the_frame is None:
+        raise
+
+    frame_flipped = flip_frame(the_frame)
+
+    frame_processed = process_frame_for_capture(the_frame)
+
+    faces_detected = detect_faces(the_frame, scale_factor, min_neigh, minSize, maxSize, flags)
+
+    display_faces(the_frame, faces_detected)
+
+    if len(faces_detected[0]) > 1:
+        continue
+
+    behave_enforces(faces_detected[0])
 
     #key handler:
     k = cv2.waitKey(1) & 0xFF
