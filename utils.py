c62b407ff08c427ee0af4af35290c168f1d78dcb
Kieleth <kieleth@gmail.com>
2014-03-30 23:12:26 +0100

    fps uses circular counter, prevents mem to grow, hopefully

--- utils.py
+++ utils.py
@@ -17,12 +17,22 @@ def prepare_frame_for_detection(frame):
     gray = cv2.equalizeHist(gray)
     return gray
 
+def circular_counter(max):
+    x = 0
+    while True:
+        if x == max:
+            x = 0
+        x += 1
+        yield x
+
 class CvTimer(object):
     def __init__(self):
         self.tick_frequency = cv2.getTickFrequency()
         self.tick_at_init = cv2.getTickCount()
         self.last_tick = self.tick_at_init
-        self.fps_history = []
+        self.fps_len = 100
+        self.l_fps_history = [ 10 for x in range(self.fps_len)]
+        self.fps_counter = circular_counter(self.fps_len)
 
     def reset(self):
         self.last_tick = cv2.getTickCount()
@@ -33,9 +43,9 @@ class CvTimer(object):
     @property    
     def fps(self):
         fps = self.tick_frequency / (self.get_tick_now() - self.last_tick)
-        self.fps_history.append(fps) 
+        self.l_fps_history[self.fps_counter.next() - 1] = fps 
         return fps
 
     @property
     def avg_fps(self):
-        return float(sum(self.fps_history)) / len(self.fps_history) 
+        return sum(self.l_fps_history) / float(self.fps_len) 
